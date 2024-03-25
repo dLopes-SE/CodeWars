ad986dd9672d0fbba409fbfd205a9c57baf8e363
Dylan Lopes <dylananlopes@gmail.com>
2024-03-25 09:13:28 +0000

    6Kyu - StopSpinningMyWords

---
+++ CodeWars/6Kyu/StopSpinningMyWords/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars._6Kyu
+{
+  public class Kata
+  {
+    public static string SpinWords(string sentence)
+    {
+      return string.Join(' ', sentence.Split(" ").Select(w => w.Length >= 5 ? new string(w.Reverse().ToArray()) : w).ToList());
+    }
+  }
+}
---
+++ CodeWars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine(_6Kyu.Kata.SpinWords("Hey fellow warriors"));
+    }
+  }
+}
