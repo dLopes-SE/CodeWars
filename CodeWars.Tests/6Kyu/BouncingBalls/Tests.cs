631f3d5debc12dd33b4a9c8f565f44ffa07efd91
Dylan Lopes <dylananlopes@gmail.com>
2024-03-27 14:21:54 +0000

    Bouncing Ball

---
+++ CodeWars.Tests/6Kyu/BouncingBalls/Tests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars._6Kyu.BouncingBalls;
+
+[Trait("Category", "6kyu_bouncingBalls")]
+public class BouncingBallTests
+{
+  [Fact]
+  public void Test1()
+  {
+    Assert.Equal(3, BouncingBall.bouncingBall(3.0, 0.66, 1.5));
+  }
+  [Fact]
+  public void Test2()
+  {
+    Assert.Equal(15, BouncingBall.bouncingBall(30.0, 0.66, 1.5));
+  }
+}
---
+++ CodeWars/6Kyu/BouncingBalls/BouncingBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace CodeWars._6Kyu.BouncingBalls
+{
+  public class BouncingBall
+  {
+    public static int bouncingBall(double h, double bounce, double window)
+    {
+      //Float parameter "h" in meters must be greater than 0 -> height of the floor
+      //Float parameter "bounce" must be greater than 0 and less than 1 -> bounce rate
+      //Float parameter "window" must be less than h. -> window's height, from where the child's mom is looking
+      if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h)
+        return -1;
+
+      var count = 1;
+      while (h*bounce > window)
+      {
+        count += 2;
+        h *= bounce;
+      }
+
+      if (h == window)
+        count++;
+
+      return count;
+    }
+  }
+}
