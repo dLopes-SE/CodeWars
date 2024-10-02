82fe8a238cccf5f6682e3e89b6b5952b0dce38fa
Dylan Lopes <dylananlopes@gmail.com>
2024-10-02 09:00:27 +0100

    5Kyu - Greed is Good

---
+++ CodeWars.Tests/5Kyu/Greed is Good/Tests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars._5Kyu.Greed_is_Good;
+
+namespace CodeWars.Tests._5Kyu.Greed_is_Good;
+
+[Trait("Category", "5Kyu_GreedIsGood")]
+public class ScoreCheckerTest
+{
+  [Fact]
+  public static void ShouldBeWorthless()
+  {
+    Assert.True(0 == ScoreChecker.Score(new int[] { 2, 3, 4, 6, 2 }));
+  }
+
+  [Fact]
+  public static void ShouldValueTriplets()
+  {
+    Assert.True(400 == ScoreChecker.Score(new int[] { 4, 4, 4, 3, 3 }));
+  }
+
+  [Fact]
+  public static void ShouldValueMixedSets()
+  {
+    Assert.True(450 == ScoreChecker.Score(new int[] { 2, 4, 4, 5, 4 }));
+  }
+}
---
+++ CodeWars/5Kyu/Greed is Good/Score.cs
@@ -0,0 +1,61 @@
+﻿namespace CodeWars._5Kyu.Greed_is_Good;
+
+public static class ScoreChecker
+{
+  public const int TRIPLE_OCCURRENCES = 3;
+  public static int Score(int[] dice)
+  {
+    var score = 0;
+
+    // Increase score + remove the sequece of 3 numbers, so that we can count the remaining at the end
+    if (dice.Count(n => n == 1) >= TRIPLE_OCCURRENCES)
+    {
+      score += 1000;
+      dice = RemoveDices(dice, 1);
+    }
+
+    // Increase score + remove the sequece of 3 numbers, so that we can count the remaining at the end
+    if (dice.Count(n => n == 5) >= TRIPLE_OCCURRENCES)
+    {
+      score += 500;
+      dice = RemoveDices(dice, 5);
+    }
+
+    if (dice.Count(n => n == 6) >= TRIPLE_OCCURRENCES)
+      score += 600;
+
+    if (dice.Count(n => n == 4) >= TRIPLE_OCCURRENCES)
+      score += 400;
+
+    if (dice.Count(n => n == 3) >= TRIPLE_OCCURRENCES)
+      score += 300;
+
+    if (dice.Count(n => n == 2) >= TRIPLE_OCCURRENCES)
+      score += 200;
+
+
+    // Now incresed the score with the remaining 1s and 5s
+    score += 100 * dice.Count(n => n == 1);
+    score += 50 * dice.Count(n => n == 5);
+
+    return score;
+  }
+
+  public static int[] RemoveDices(int[] dice, int no)
+  {
+    var countRemoved = 0;
+    return dice.Where(n =>
+    {
+      if (n != no)
+        return true;
+
+      if (countRemoved < TRIPLE_OCCURRENCES)
+      {
+        countRemoved++;
+        return false;
+      }
+
+      return true;
+    }).ToArray();
+  }
+}
